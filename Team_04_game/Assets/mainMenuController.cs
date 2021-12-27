472147f880f8867847aa10d8d6d5aeccabf14a19
J.R.C.M. van Dijke <j.r.c.m.van.dijke@umail.leidenuniv.nl>
2021-12-27 13:21:07 +0100

    menu

---
+++ Team_04_game/Assets/mainMenuController.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class mainMenuController : MonoBehaviour
+{
+
+    public void startGame() {
+        SceneManager.LoadScene("Scenes/Implementation 1.unity");
+    }
+    
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
